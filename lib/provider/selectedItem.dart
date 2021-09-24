259faa5a44c680e69deade972ec3ada70f4fe89c
YoussifHany51 <youssifhany962@gmail.com>
2021-09-25 00:32:38 +0200

    Editing some functions

--- lib/provider/selectedItem.dart
+++ lib/provider/selectedItem.dart
@@ -4,14 +4,20 @@ import 'package:flutter/widgets.dart';
 class SelectedItem extends ChangeNotifier {
   List<Meal> meals = [];
   int totalCal = 0;
+  int currentKcal = 0;
   addItem(Meal meal) {
     meals.add(meal);
     totalCal += conertToInt(meal.kcal);
+    currentKcal = conertToInt(meal.kcal);
     notifyListeners();
   }
 
   int get totalCalories {
     return totalCal;
+  }
+
+  int get currKcalFunction {
+    return currentKcal;
   }
 
   int conertToInt(String value) {
