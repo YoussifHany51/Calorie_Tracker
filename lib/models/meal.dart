3b938f11cd92b4c6a011088dacbfa0959d8829bf
YoussifHany51 <youssifhany962@gmail.com>
2021-10-13 12:42:36 +0200

    Edit meal model

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,14 +1,9 @@
-import 'package:hive/hive.dart';
-
-//part 'meal.g.dart';
-
-@HiveType(typeId: 0)
-class Meal extends HiveObject {
-  @HiveField(0)
+class Meal {
+  late int id;
   late String name;
-  @HiveField(1)
-  var kcal;
-  @HiveField(2)
+
+  late var kcal;
+
   late DateTime? date;
   Meal({
     required this.name,
@@ -16,23 +11,31 @@ class Meal extends HiveObject {
     this.date,
   });
 
-  // Map<String, dynamic> toMap() {
-  //   final map = Map<String, dynamic>();
-  //   // ignore: unnecessary_null_comparison
-  //   if (id != null) {
-  //     map['id'] = id;
-  //   }
-  //   map['id'] = id;
-  //   map['name'] = name;
-  //   map['kcal'] = kcal;
-  //   return map;
-  // }
+  Meal.withId({
+    required this.id,
+    required this.name,
+    required this.kcal,
+    this.date,
+  });
 
-  // factory Meal.fromMap(Map<String, dynamic> map) {
-  //   return Meal.withId(
-  //     id: map['id'],
-  //     name: map['name'],
-  //     kcal: map['kcal'],
-  //   );
-  // }
+  Map<String, dynamic> toMap() {
+    final map = Map<String, dynamic>();
+    // ignore: unnecessary_null_comparison
+
+    if (id != null) {
+      map['id'] = id;
+    }
+    map['id'] = id;
+    map['name'] = name;
+    map['kcal'] = kcal;
+    return map;
+  }
+
+  factory Meal.fromMap(Map<String, dynamic> map) {
+    return Meal.withId(
+      id: map['id'],
+      name: map['name'],
+      kcal: map['kcal'],
+    );
+  }
 }
